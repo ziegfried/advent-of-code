aa0a99bd8d1b9c447d1409b8d304fd1f80850594
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2021-12-11 23:22:15 -0800

    cleanup

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn parse_graph(input: &str) -> Vec<(String, String)> {
     input
@@ -10,14 +10,14 @@ fn parse_graph(input: &str) -> Vec<(String, String)> {
         .collect::<Vec<_>>()
 }
 
-fn walk(graph: &Vec<(String, String)>, el: String, stack: &Vec<String>) -> usize {
+fn walk(graph: &Vec<(String, String)>, el: String, path: &Vec<String>) -> usize {
     if el.to_lowercase() == el {
-        if stack.iter().find(|s| s == &&el).is_some() {
+        if path.iter().find(|s| s == &&el).is_some() {
             return 0;
         }
     }
-    let mut stack = stack.clone();
-    stack.push(el.clone());
+    let mut path = path.clone();
+    path.push(el.clone());
     let routes = graph
         .iter()
         .filter_map(|(start, end)| {
@@ -36,7 +36,7 @@ fn walk(graph: &Vec<(String, String)>, el: String, stack: &Vec<String>) -> usize
         if *route == "end".to_string() {
             paths += 1;
         } else {
-            paths += walk(graph, route.clone(), &stack.clone());
+            paths += walk(graph, route.clone(), &path.clone());
         }
     }
     paths
@@ -48,42 +48,29 @@ fn part1(input: &str) -> usize {
 }
 
 fn walk2(
-    graph: &Vec<(String, String)>,
+    graph_map: &HashMap<String, Vec<String>>,
     el: String,
-    stack: &Vec<String>,
+    path: &Vec<String>,
     all_paths: &mut HashSet<String>,
     extra: Option<String>,
 ) {
     if el.to_lowercase() == el {
-        let count = stack.iter().filter(|s| s == &&el).count();
+        let count = path.iter().filter(|s| s == &&el).count();
         let target_count = if Some(el.clone()) == extra { 2 } else { 1 };
         if count == target_count {
             return;
         }
     }
-    let mut stack = stack.clone();
-    stack.push(el.clone());
-    let routes = graph
-        .iter()
-        .filter_map(|(start, end)| {
-            if start == &el {
-                Some(end)
-            } else if end == &el {
-                Some(start)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    for route in routes {
-        if *route == "end".to_string() {
-            all_paths.insert(stack.join("-"));
+    let mut path = path.clone();
+    path.push(el.clone());
+    for next_route in graph_map.get(&el).unwrap() {
+        if *next_route == "end".to_string() {
+            all_paths.insert(path.join("-"));
         } else {
             walk2(
-                graph,
-                route.clone(),
-                &stack.clone(),
+                graph_map,
+                next_route.clone(),
+                &path,
                 all_paths,
                 extra.clone(),
             );
@@ -93,17 +80,46 @@ fn walk2(
 
 fn part2(input: &str) -> usize {
     let graph = parse_graph(input);
-    let lowers = graph
+    let all_nodes = graph
         .iter()
-        .flat_map(|(a, b)| vec![a, b])
+        .flat_map(|(a, b)| vec![a.clone(), b.clone()])
+        .collect::<Vec<String>>();
+    let graph_map: HashMap<String, Vec<String>> = all_nodes
+        .iter()
+        .map(|e| {
+            (
+                e.clone(),
+                graph
+                    .iter()
+                    .filter_map(|(start, end)| {
+                        if start == e {
+                            Some(end.clone())
+                        } else if end == e {
+                            Some(start.clone())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<String>>(),
+            )
+        })
+        .collect();
+    let lower_nodes = all_nodes
+        .iter()
         .filter(|v| v != &&"start" && v != &&"end")
         .filter(|v| v == &&v.to_lowercase())
         .collect::<HashSet<_>>();
     let mut all_paths = HashSet::<String>::new();
-    walk2(&graph, "start".to_string(), &vec![], &mut all_paths, None);
-    for l in lowers {
+    walk2(
+        &graph_map,
+        "start".to_string(),
+        &vec![],
+        &mut all_paths,
+        None,
+    );
+    for l in lower_nodes {
         walk2(
-            &graph,
+            &graph_map,
             "start".to_string(),
             &vec![],
             &mut all_paths,
