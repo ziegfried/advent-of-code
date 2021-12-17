44b0447dd81eb88dd536c8703a9c901fee97b20d
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2021-12-16 22:21:21 -0800

    cleanup

--- 2021/day17/src/main.rs
+++ 2021/day17/src/main.rs
@@ -1,3 +1,5 @@
+type TargetArea = ((i32, i32), (i32, i32));
+
 #[derive(Debug)]
 struct State {
     x: i32,
@@ -6,28 +8,29 @@ struct State {
     vy: i32,
 }
 
-type TargetArea = ((i32, i32), (i32, i32));
-
+#[inline(always)]
 fn step(state: State) -> State {
     State {
         x: state.x + state.vx,
         y: state.y + state.vy,
-        vx: state.vx + (state.vx.signum() * -1),
+        vx: state.vx - state.vx.signum(),
         vy: state.vy - 1,
     }
 }
 
-fn contains(((x1, x2), (y1, y2)): TargetArea, x: i32, y: i32) -> bool {
-    x >= x1 && x <= x2 && y >= y1 && y <= y2
+#[inline(always)]
+fn contains(((x_min, x_max), (y_min, y_max)): TargetArea, x: i32, y: i32) -> bool {
+    x >= x_min && x <= x_max && y >= y_min && y <= y_max
 }
 
-fn out_of_range(((_, x2), (y1, _)): TargetArea, x: i32, y: i32, vy: i32) -> bool {
-    x > x2 || (y < y1 && vy <= 0)
+#[inline(always)]
+fn out_of_range(((_, x_max), (y_min, _)): TargetArea, x: i32, y: i32, vy: i32) -> bool {
+    x > x_max || (y < y_min && vy <= 0)
 }
 
 fn check_hit(vx: i32, vy: i32, target: TargetArea) -> Option<i32> {
     let mut state = State { x: 0, y: 0, vx, vy };
-    let mut max_y = 0;
+    let mut max_y = i32::MIN;
     loop {
         state = step(state);
         max_y = i32::max(max_y, state.y);
@@ -40,43 +43,34 @@ fn check_hit(vx: i32, vy: i32, target: TargetArea) -> Option<i32> {
     }
 }
 
-fn part1(target: TargetArea) -> i32 {
+fn parts(target: TargetArea) -> (i32, i32) {
     let mut max_y = i32::MIN;
-    for x in 0..1000 {
-        for y in -500..1000 {
-            if let Some(y) = check_hit(x, y, target) {
+    let mut hit_count = 0;
+    for vx in 0..1000 {
+        for vy in -500..1000 {
+            if let Some(y) = check_hit(vx, vy, target) {
+                hit_count += 1;
                 if y > max_y {
                     max_y = y;
                 }
             }
         }
     }
-    max_y
-}
-
-fn part2(target: TargetArea) -> i32 {
-    let mut hit_count = 0;
-    for x in 0..1000 {
-        for y in -500..1000 {
-            if check_hit(x, y, target).is_some() {
-                hit_count += 1;
-            }
-        }
-    }
-    hit_count
+    (max_y, hit_count)
 }
 
 fn main() {
-    println!("Part 1: {:?}", part1(((288, 330), (-96, -50))));
-    println!("Part 2: {:?}", part2(((288, 330), (-96, -50))));
+    let (part1, part2) = parts(((288, 330), (-96, -50)));
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[test]
 fn test_part1() {
-    assert_eq!(part1(((20, 30), (-10, -5))), 45);
+    assert_eq!(parts(((20, 30), (-10, -5))).0, 45);
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(((20, 30), (-10, -5))), 112);
+    assert_eq!(parts(((20, 30), (-10, -5))).1, 112);
 }
