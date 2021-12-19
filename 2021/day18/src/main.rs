66b80d83e8f8b1474f9ed469734cf86913198f00
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2021-12-18 17:11:11 -0800

    small improvements

--- 2021/day18/src/main.rs
+++ 2021/day18/src/main.rs
@@ -1,10 +1,21 @@
 use itertools::Itertools;
-use serde_json::Value;
+use std::fmt::{self, Debug, Formatter};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 enum Number {
     Reg(usize),
     Pair(Box<Number>, Box<Number>),
+}
+impl Debug for Number {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        fn number_to_string(n: &Number) -> String {
+            match n {
+                Reg(v) => format!("{}", v),
+                Pair(a, b) => format!("[{},{}]", number_to_string(a), number_to_string(b)),
+            }
+        }
+        formatter.write_str(&number_to_string(self))
+    }
 }
 use Number::*;
 
@@ -12,21 +23,19 @@ fn pair(n1: Number, n2: Number) -> Number {
     Pair(Box::new(n1), Box::new(n2))
 }
 
-fn map_json_value(value: &Value) -> Number {
-    use Value::*;
-    match value {
-        Number(n) => Reg(n.as_u64().unwrap() as usize),
-        Array(children) => {
-            assert_eq!(children.len(), 2);
-            pair(map_json_value(&children[0]), map_json_value(&children[1]))
-        }
-        _ => panic!(),
-    }
-}
-
 fn parse(input: &str) -> Number {
-    let v: Value = serde_json::from_str(input).unwrap();
-    map_json_value(&v)
+    fn map_json_value(value: &serde_json::Value) -> Number {
+        use serde_json::Value::*;
+        match value {
+            Number(n) => Reg(n.as_u64().unwrap() as usize),
+            Array(children) => {
+                assert_eq!(children.len(), 2);
+                pair(map_json_value(&children[0]), map_json_value(&children[1]))
+            }
+            _ => panic!("invalid number string: unexpected value type {:?}", value),
+        }
+    }
+    map_json_value(&serde_json::from_str(input).expect("invalid number string"))
 }
 
 fn split(n: &Number) -> Option<Number> {
@@ -50,55 +59,47 @@ fn split(n: &Number) -> Option<Number> {
     }
 }
 
-fn add(n1: &Number, n2: &Number) -> Number {
-    pair(n1.clone(), n2.clone())
-}
-
-fn unwrap_regular(n: &Number) -> usize {
-    match n {
-        Reg(v) => *v,
-        _ => panic!("{:?} not a regular number", n),
-    }
-}
-
-fn add_to_side(n: &Number, val: usize, left_side: bool) -> Number {
-    match n {
-        Reg(v) => Reg(v + val),
-        Pair(l, r) => match left_side {
-            true => Pair(Box::new(add_to_side(l, val, true)), r.clone()),
-            false => Pair(l.clone(), Box::new(add_to_side(r, val, false))),
-        },
-    }
-}
-
-fn explode_layer(n: &Number, depth: usize) -> Option<(Number, usize, usize)> {
-    if depth == 4 {
-        if let Pair(a, b) = n {
-            return Some((Reg(0), unwrap_regular(a), unwrap_regular(b)));
-        }
-    }
-    if let Pair(left, right) = n {
-        if let Some((left, left_over, right_over)) = explode_layer(left, depth + 1) {
-            let right = if let Reg(n) = **right {
-                Reg(n + right_over)
-            } else {
-                add_to_side(right, right_over, true)
-            };
-            return Some((Pair(Box::new(left), Box::new(right)), left_over, 0));
-        }
-        if let Some((right, left_over, right_over)) = explode_layer(right, depth + 1) {
-            let left = if let Reg(n) = **left {
-                Reg(n + left_over)
-            } else {
-                add_to_side(left, left_over, false)
-            };
-            return Some((Pair(Box::new(left), Box::new(right)), 0, right_over));
-        }
-    }
-    None
-}
-
 fn explode(n: &Number) -> Option<Number> {
+    fn add_to_side(n: &Number, val: usize, left_side: bool) -> Number {
+        match n {
+            Reg(v) => Reg(v + val),
+            Pair(l, r) => match left_side {
+                true => Pair(Box::new(add_to_side(l, val, true)), r.clone()),
+                false => Pair(l.clone(), Box::new(add_to_side(r, val, false))),
+            },
+        }
+    }
+    fn unwrap_regular(n: &Number) -> usize {
+        match n {
+            Reg(v) => *v,
+            _ => panic!("{:?} not a regular number", n),
+        }
+    }
+    fn explode_layer(n: &Number, depth: usize) -> Option<(Number, usize, usize)> {
+        if let Pair(left, right) = n {
+            if depth == 4 {
+                return Some((Reg(0), unwrap_regular(left), unwrap_regular(right)));
+            }
+            if let Some((left, left_over, right_over)) = explode_layer(left, depth + 1) {
+                let right = if let Reg(n) = **right {
+                    Reg(n + right_over)
+                } else {
+                    add_to_side(right, right_over, true)
+                };
+                return Some((Pair(Box::new(left), Box::new(right)), left_over, 0));
+            }
+            if let Some((right, left_over, right_over)) = explode_layer(right, depth + 1) {
+                let left = if let Reg(n) = **left {
+                    Reg(n + left_over)
+                } else {
+                    add_to_side(left, left_over, false)
+                };
+                return Some((Pair(Box::new(left), Box::new(right)), 0, right_over));
+            }
+        }
+        None
+    }
+
     explode_layer(n, 0).map(|(res, _, _)| res)
 }
 
@@ -112,6 +113,10 @@ fn reduce(n: &Number) -> Number {
     }
 }
 
+fn add(n1: &Number, n2: &Number) -> Number {
+    reduce(&pair(n1.clone(), n2.clone()))
+}
+
 fn magnitude(v: &Number) -> usize {
     match v {
         Pair(a, b) => 3 * magnitude(a) + 2 * magnitude(b),
@@ -119,20 +124,8 @@ fn magnitude(v: &Number) -> usize {
     }
 }
 
-#[allow(unused)]
-fn number_to_str(n: &Number) -> String {
-    match n {
-        Reg(v) => format!("{}", v),
-        Pair(a, b) => format!("[{},{}]", number_to_str(a), number_to_str(b)),
-    }
-}
-
 fn part1(input: &str) -> usize {
-    let result = input
-        .lines()
-        .map(parse)
-        .reduce(|a, b| reduce(&add(&a, &b)))
-        .unwrap();
+    let result = input.lines().map(parse).reduce(|a, b| add(&a, &b)).unwrap();
     magnitude(&result)
 }
 
@@ -141,7 +134,7 @@ fn part2(input: &str) -> usize {
         .lines()
         .map(parse)
         .permutations(2)
-        .map(|perm| magnitude(&reduce(&add(&perm[0], &perm[1]))))
+        .map(|perm| magnitude(&add(&perm[0], &perm[1])))
         .max()
         .unwrap()
 }
