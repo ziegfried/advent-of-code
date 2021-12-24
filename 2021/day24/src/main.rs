47a5b4d0a0461df102aaf8cb7f04f2ae86ce6f9a
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2021-12-24 14:25:28 -0800

    tests

--- 2021/day24/src/main.rs
+++ 2021/day24/src/main.rs
@@ -213,3 +213,13 @@ fn main() {
     println!("Part 1: {}", part1(include_str!("in.txt")));
     println!("Part 2: {}", part2(include_str!("in.txt")));
 }
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(include_str!("in.txt")), 49917929934999);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("in.txt")), 11911316711816);
+}
