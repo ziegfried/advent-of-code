bdff799d851fb645035cf7c6fc921d35ef05ce2f
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2021-12-27 21:31:20 -0800

    simplified

--- 2021/day24/src/main.rs
+++ 2021/day24/src/main.rs
@@ -130,22 +130,18 @@ fn execute(instructions: &[Instruction], input: i64, variables: &mut [i64; 4]) -
 
 fn split_into_chunks(instructions: &Vec<Instruction>) -> Vec<Vec<Instruction>> {
     let mut result = vec![];
-    let mut idx = 0;
+    let mut input_idx = 0;
     loop {
-        if let Some((next, _)) = instructions
+        assert!(matches!(instructions[input_idx], Inp(_)));
+        if let Some((next_input, _)) = instructions
             .iter()
             .enumerate()
-            .find(|(i, inst)| i > &idx && matches!(inst, Inp(_)))
+            .find(|(i, inst)| i > &input_idx && matches!(inst, Inp(_)))
         {
-            result.push(
-                instructions[idx..next]
-                    .into_iter()
-                    .map(|i| i.clone())
-                    .collect(),
-            );
-            idx = next;
+            result.push(instructions[input_idx..next_input].into_iter().cloned().collect());
+            input_idx = next_input;
         } else {
-            result.push(instructions[idx..].into_iter().map(|i| i.clone()).collect());
+            result.push(instructions[input_idx..].into_iter().cloned().collect());
             break;
         }
     }
@@ -153,41 +149,39 @@ fn split_into_chunks(instructions: &Vec<Instruction>) -> Vec<Vec<Instruction>> {
 }
 
 fn find_input(
-    chunk: usize,
+    chunks: &[Vec<Instruction>],
     vars: [i64; 4],
-    chunks: &Vec<Vec<Instruction>>,
     digits: &Vec<i64>,
     dead_ends: &mut HashSet<(usize, i64)>,
-    value: i64,
+    model_number: i64,
 ) -> Option<i64> {
-    if dead_ends.contains(&(chunk, read_var(Z, &vars))) {
+    if dead_ends.contains(&(chunks.len(), read_var(Z, &vars))) {
         return None;
     }
-    let instructions = &chunks[chunk];
+    let instructions = &chunks[0];
     for d in digits {
-        let value = value * 10 + d;
-        let mut new_vars = vars.clone();
-        let z = execute(instructions, *d, &mut new_vars);
-        if chunk == chunks.len() - 1 {
+        let mut vars = vars.clone();
+        let model_number = model_number * 10 + d;
+        let z = execute(instructions, *d, &mut vars);
+        let chunks = &chunks[1..];
+        if chunks.is_empty() {
             if z == 0 {
-                return Some(value);
+                return Some(model_number);
             }
-        } else if let Some(res) = find_input(chunk + 1, new_vars, chunks, digits, dead_ends, value)
-        {
+        } else if let Some(res) = find_input(chunks, vars, digits, dead_ends, model_number) {
             return Some(res);
         }
     }
-    dead_ends.insert((chunk, read_var(Z, &vars)));
+    dead_ends.insert((chunks.len(), read_var(Z, &vars)));
     None
 }
 
 fn part1(input: &str) -> i64 {
     let instructions = parse(input);
-    let chunks = split_into_chunks(&instructions);
+    let instruction_chunks = split_into_chunks(&instructions);
     find_input(
-        0,
-        Default::default(),
-        &chunks,
+        &instruction_chunks,
+        [0; 4],
         &(1..=9).rev().collect(),
         &mut HashSet::new(),
         0,
@@ -197,11 +191,10 @@ fn part1(input: &str) -> i64 {
 
 fn part2(input: &str) -> i64 {
     let instructions = parse(input);
-    let chunks = split_into_chunks(&instructions);
+    let instruction_chunks = split_into_chunks(&instructions);
     find_input(
-        0,
-        Default::default(),
-        &chunks,
+        &instruction_chunks,
+        [0; 4],
         &(1..=9).collect(),
         &mut HashSet::new(),
         0,
