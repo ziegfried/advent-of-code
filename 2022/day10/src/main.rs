6fba64ccd667d82ec86eb0ace533bde9fe621199
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2022-12-09 23:57:39 -0800

    simplified

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -1,71 +1,61 @@
 // Problem: https://adventofcode.com/2022/day/10
 
+use std::cell::Cell;
+
 fn part1(input: &str) -> usize {
-    struct System {
-        cycle: usize,
-        x: i32,
-        result: usize,
-    }
-    let mut sys = System {
-        cycle: 0,
-        x: 1,
-        result: 0,
-    };
-    fn inc(sys: &mut System) {
-        sys.cycle += 1;
-        if (sys.cycle as i32 - 20) % 40 == 0 {
-            sys.result += sys.x as usize * sys.cycle;
+    let mut cycle = 0;
+    let x: Cell<i32> = Cell::new(1);
+    let mut result = 0;
+
+    let mut inc = || {
+        cycle += 1;
+        if (cycle as i32 - 20) % 40 == 0 {
+            result += x.get() as usize * cycle;
         }
-    }
+    };
 
     for inst in input.trim().lines() {
         if inst == "noop" {
-            inc(&mut sys);
+            inc();
         } else {
-            inc(&mut sys);
+            inc();
             let (_, amount) = inst.split_once(' ').unwrap();
             let amount: i32 = amount.parse().unwrap();
-            inc(&mut sys);
-            sys.x += amount;
+            inc();
+            x.replace(x.get() + amount);
         }
     }
 
-    sys.result
+    result
 }
 
 fn part2(input: &str) -> String {
-    struct System {
-        cycle: usize,
-        x: i32,
-        crt: Vec<Vec<char>>,
-    }
-    let mut sys = System {
-        cycle: 0,
-        x: 1,
-        crt: vec![vec![' '; 40]; 6],
+    let mut cycle = 0;
+    let x: Cell<i32> = Cell::new(1);
+    let mut crt = vec![vec![' '; 40]; 6];
+
+    let mut inc = || {
+        let x = x.get();
+        let row = cycle / 40;
+        let pos = cycle % 40;
+        let is_sprite_drawn = ((x - 1)..=(x + 1)).contains(&(pos as i32));
+        crt[row][pos] = if is_sprite_drawn { '#' } else { '.' };
+        cycle += 1;
     };
-    fn inc(sys: &mut System) {
-        let row = sys.cycle / 40;
-        let pos = sys.cycle % 40;
-        let is_sprite_drawn = ((sys.x - 1)..=(sys.x + 1)).contains(&(pos as i32));
-        sys.crt[row][pos] = if is_sprite_drawn { '#' } else { '.' };
-        sys.cycle += 1;
-    }
 
     for inst in input.trim().lines() {
         if inst == "noop" {
-            inc(&mut sys);
+            inc();
         } else {
-            inc(&mut sys);
+            inc();
             let (_, amount) = inst.split_once(' ').unwrap();
             let amount: i32 = amount.parse().unwrap();
-            inc(&mut sys);
-            sys.x += amount;
+            inc();
+            x.replace(x.get() + amount);
         }
     }
 
-    sys.crt
-        .iter()
+    crt.iter()
         .map(|line| line.iter().collect::<String>())
         .collect::<Vec<_>>()
         .join("\n")
