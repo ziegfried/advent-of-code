6868b2e79fb9478a29eb95af9cefe16f4e6885ba
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2022-12-22 01:11:35 -0800

    cleanup and cube shape for test case

--- 2022/day22/src/main.rs
+++ 2022/day22/src/main.rs
@@ -1,14 +1,6 @@
-#![allow(
-    dead_code,
-    unused_variables,
-    unreachable_code,
-    clippy::diverging_sub_expression
-)]
-
-use std::collections::HashMap;
-
 // Problem: https://adventofcode.com/2022/day/22
 use itertools::Itertools;
+use std::collections::HashMap;
 use Direction::*;
 
 type Point = (i32, i32);
@@ -167,93 +159,36 @@ impl Board {
             .into_option()
             .unwrap()
     }
-
-    fn bounds(&self) -> (Point, Point) {
-        let (&min_row, &max_row) = self
-            .0
-            .keys()
-            .map(|(row, _)| row)
-            .minmax()
-            .into_option()
-            .unwrap();
-        let (&min_col, &max_col) = self
-            .0
-            .keys()
-            .map(|(_, col)| col)
-            .minmax()
-            .into_option()
-            .unwrap();
-
-        ((min_row, min_col), (max_row, max_col))
-    }
-
-    fn print(&self, state: &State) {
-        let ((min_row, min_col), (max_row, max_col)) = self.bounds();
-
-        for row in min_row..=max_row {
-            for col in min_col..=max_col {
-                let p = (row, col);
-                if p == state.pos {
-                    match state.facing {
-                        Up => print!("^"),
-                        Right => print!(">"),
-                        Down => print!("v"),
-                        Left => print!("<"),
-                    }
-                } else {
-                    match self.get(p) {
-                        Space::Empty => print!(" "),
-                        Space::Open => print!("."),
-                        Space::Wall => print!("#"),
-                    }
-                }
-            }
-            println!();
-        }
-    }
-    fn print_with_moves(&self, moves: &HashMap<Point, Direction>) {
-        let ((min_row, min_col), (max_row, max_col)) = self.bounds();
-
-        for row in min_row..=max_row {
-            for col in min_col..=max_col {
-                let p = (row, col);
-                if let Some(dir) = moves.get(&p) {
-                    match dir {
-                        Up => print!("^"),
-                        Right => print!(">"),
-                        Down => print!("v"),
-                        Left => print!("<"),
-                    }
-                } else {
-                    match self.get(p) {
-                        Space::Empty => print!(" "),
-                        Space::Open => print!("."),
-                        Space::Wall => print!("#"),
-                    }
-                }
-            }
-            println!();
-        }
-    }
 }
 
-fn part1(input: &str) -> i32 {
-    let (board, instructions) = input.split_once("\n\n").unwrap();
-    let board = parse_board(board);
-    let instructions = parse_instructions(instructions.trim());
+type WrapFn = fn(Point, Direction, &Board, i32) -> (Point, Direction);
 
+fn wrap((row, col): Point, dir: Direction, board: &Board, _: i32) -> (Point, Direction) {
+    let next = match dir {
+        Up => (board.col_bounds(col).1, col),
+        Right => (row, board.row_bounds(row).0),
+        Down => (board.col_bounds(col).0, col),
+        Left => (row, board.row_bounds(row).1),
+    };
+
+    (next, dir)
+}
+
+fn make_moves(
+    board: &Board,
+    instructions: Vec<Instruction>,
+    wrap_around: WrapFn,
+    sqs: i32,
+) -> State {
     let mut state = State {
         pos: (0, board.row_bounds(0).0),
         facing: Direction::Right,
     };
 
-    let mut moves = HashMap::new();
-
     for instruction in instructions {
         match instruction {
             Instruction::Go(distance) => {
                 for _ in 0..distance {
-                    moves.insert(state.pos, state.facing);
                     let pos = state.pos;
                     let facing = state.facing;
                     let next_pos = facing.go(&pos);
@@ -268,90 +203,7 @@ fn part1(input: &str) -> i32 {
                             break;
                         }
                         Space::Empty => {
-                            let next = match state.facing {
-                                Up => (board.col_bounds(state.pos.1).1, state.pos.1),
-                                Right => (state.pos.0, board.row_bounds(state.pos.0).0),
-                                Down => (board.col_bounds(state.pos.1).0, state.pos.1),
-                                Left => (state.pos.0, board.row_bounds(state.pos.0).1),
-                            };
-                            match board.get(next) {
-                                Space::Empty => panic!("wrap around is empty"),
-                                Space::Open => {
-                                    state = State { pos: next, facing };
-                                }
-                                Space::Wall => {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    moves.insert(state.pos, state.facing);
-                }
-            }
-            Instruction::Turn(turn) => match turn {
-                Turn::Right => {
-                    state = State {
-                        pos: state.pos,
-                        facing: state.facing.turn_right(),
-                    }
-                }
-                Turn::Left => {
-                    state = State {
-                        pos: state.pos,
-                        facing: state.facing.turn_left(),
-                    }
-                }
-            },
-        }
-        moves.insert(state.pos, state.facing);
-    }
-
-    // board.print_with_moves(&moves);
-
-    let (row, col) = state.pos;
-    let face_value: i32 = match state.facing {
-        Up => 3,
-        Right => 0,
-        Down => 1,
-        Left => 2,
-    };
-
-    (row + 1) * 1000 + (col + 1) * 4 + face_value
-}
-
-fn part2(input: &str, square_size: i32) -> i32 {
-    let (board, instructions) = input.split_once("\n\n").unwrap();
-    let board = parse_board(board);
-    let instructions = parse_instructions(instructions.trim());
-
-    let mut state = State {
-        pos: (0, board.row_bounds(0).0),
-        facing: Direction::Right,
-    };
-
-    let mut moves = HashMap::new();
-
-    for instruction in instructions {
-        match instruction {
-            Instruction::Go(distance) => {
-                for _ in 0..distance {
-                    moves.insert(state.pos, state.facing);
-                    let pos = state.pos;
-                    let facing = state.facing;
-                    let next_pos = facing.go(&pos);
-                    match board.get(next_pos) {
-                        Space::Open => {
-                            state = State {
-                                pos: next_pos,
-                                facing,
-                            };
-                        }
-                        Space::Wall => {
-                            break;
-                        }
-                        Space::Empty => {
-                            let (next, next_dir) =
-                                wrap_on_cube(state.pos, state.facing, &board, square_size);
+                            let (next, next_dir) = wrap_around(state.pos, state.facing, board, sqs);
                             match board.get(next) {
                                 Space::Empty => panic!("wrap around is empty"),
                                 Space::Open => {
@@ -366,7 +218,6 @@ fn part2(input: &str, square_size: i32) -> i32 {
                             }
                         }
                     }
-                    moves.insert(state.pos, state.facing);
                 }
             }
             Instruction::Turn(turn) => match turn {
@@ -384,11 +235,11 @@ fn part2(input: &str, square_size: i32) -> i32 {
                 }
             },
         }
-        moves.insert(state.pos, state.facing);
     }
+    state
+}
 
-    // board.print_with_moves(&moves);
-
+fn compute_password(state: State) -> i32 {
     let (row, col) = state.pos;
     let face_value: i32 = match state.facing {
         Up => 3,
@@ -396,21 +247,27 @@ fn part2(input: &str, square_size: i32) -> i32 {
         Down => 1,
         Left => 2,
     };
-
     (row + 1) * 1000 + (col + 1) * 4 + face_value
 }
 
-fn wrap_on_cube((row, col): Point, dir: Direction, board: &Board, sqs: i32) -> (Point, Direction) {
-    let seg = (row / sqs, col / sqs);
+fn part1(input: &str) -> i32 {
+    let (board, instructions) = input.split_once("\n\n").unwrap();
+    let board = parse_board(board);
+    let instructions = parse_instructions(instructions.trim());
 
-    let translate = |(row, col): Point,
-                     src_seg: Point,
-                     src_dir: Direction,
-                     dst_seg: Point,
-                     dst_dir: Direction,
-                     flip: bool|
-     -> (Point, Direction) {
-        let mut n = match src_dir {
+    let state = make_moves(&board, instructions, wrap, 0);
+    compute_password(state)
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(include_str!("test.txt")), 6032);
+}
+
+fn wrap_on_cube((row, col): Point, dir: Direction, _board: &Board, sqs: i32) -> (Point, Direction) {
+    let seg = (row / sqs, col / sqs);
+    let translate = |dst_seg: Point, dest_side: Direction, flip: bool| -> (Point, Direction) {
+        let mut n = match dir {
             Up => col,
             Right => row,
             Down => col,
@@ -419,57 +276,70 @@ fn wrap_on_cube((row, col): Point, dir: Direction, board: &Board, sqs: i32) -> (
         if flip {
             n = sqs - n - 1;
         }
-
-        let dst_pos = match dst_dir {
+        let dest = match dest_side {
             Up => (dst_seg.0 * sqs, dst_seg.1 * sqs + n),
             Right => (dst_seg.0 * sqs + n, (dst_seg.1 + 1) * sqs - 1),
             Down => ((dst_seg.0 + 1) * sqs - 1, dst_seg.1 * sqs + n),
             Left => (dst_seg.0 * sqs + n, dst_seg.1 * sqs),
         };
-
-        (dst_pos, dst_dir.turn_around())
+        (dest, dest_side.turn_around())
     };
-
     if sqs == 50 {
         match (seg, dir) {
-            ((0, 1), Up) => translate((row, col), seg, dir, (3, 0), Left, false),
-            ((0, 1), Left) => translate((row, col), seg, dir, (2, 0), Left, true),
-            ((0, 2), Up) => translate((row, col), seg, dir, (3, 0), Down, false),
-            ((0, 2), Right) => translate((row, col), seg, dir, (2, 1), Right, true),
-            ((0, 2), Down) => translate((row, col), seg, dir, (1, 1), Right, false),
-            ((1, 1), Left) => translate((row, col), seg, dir, (2, 0), Up, false),
-            ((1, 1), Right) => translate((row, col), seg, dir, (0, 2), Down, false),
-            ((2, 0), Up) => translate((row, col), seg, dir, (1, 1), Left, false),
-            ((2, 0), Left) => translate((row, col), seg, dir, (0, 1), Left, true),
-            ((2, 1), Right) => translate((row, col), seg, dir, (0, 2), Right, true),
-            ((2, 1), Down) => translate((row, col), seg, dir, (3, 0), Right, false),
-            ((3, 0), Left) => translate((row, col), seg, dir, (0, 1), Up, false),
-            ((3, 0), Right) => translate((row, col), seg, dir, (2, 1), Down, false),
-            ((3, 0), Down) => translate((row, col), seg, dir, (0, 2), Up, false),
+            ((0, 1), Up) => translate((3, 0), Left, false),
+            ((0, 1), Left) => translate((2, 0), Left, true),
+            ((0, 2), Up) => translate((3, 0), Down, false),
+            ((0, 2), Right) => translate((2, 1), Right, true),
+            ((0, 2), Down) => translate((1, 1), Right, false),
+            ((1, 1), Left) => translate((2, 0), Up, false),
+            ((1, 1), Right) => translate((0, 2), Down, false),
+            ((2, 0), Up) => translate((1, 1), Left, false),
+            ((2, 0), Left) => translate((0, 1), Left, true),
+            ((2, 1), Right) => translate((0, 2), Right, true),
+            ((2, 1), Down) => translate((3, 0), Right, false),
+            ((3, 0), Left) => translate((0, 1), Up, false),
+            ((3, 0), Right) => translate((2, 1), Down, false),
+            ((3, 0), Down) => translate((0, 2), Up, false),
             _ => panic!("invalid wrap {:?} {:?}", seg, dir),
         }
-    // } else {
-    //     match (seg, dir) {
-    //         ((0, 3), Left) => translate((row, col), seg, dir, (3, 0), Left, false),
-    //         _ => panic!("invalid wrap {:?} {:?}", seg, dir),
-    //     }
-
+    } else if sqs == 4 {
+        match (seg, dir) {
+            ((0, 2), Left) => translate((1, 1), Up, false),
+            ((0, 2), Up) => translate((1, 0), Up, true),
+            ((0, 2), Right) => translate((2, 3), Right, true),
+            ((1, 0), Left) => translate((2, 3), Down, true),
+            ((1, 0), Up) => translate((0, 2), Up, true),
+            ((1, 0), Down) => translate((2, 2), Down, true),
+            ((1, 1), Up) => translate((0, 2), Left, false),
+            ((1, 1), Down) => translate((2, 2), Left, true),
+            ((1, 2), Right) => translate((2, 3), Up, true),
+            ((2, 2), Left) => translate((1, 1), Down, true),
+            ((2, 2), Down) => translate((1, 0), Down, true),
+            ((2, 3), Up) => translate((1, 2), Right, true),
+            ((2, 3), Right) => translate((0, 2), Right, true),
+            ((2, 3), Down) => translate((1, 0), Left, true),
+            _ => panic!("invalid wrap {:?} {:?}", seg, dir),
+        }
     } else {
         panic!("unknown cube size")
     }
 }
 
-fn main() {
-    println!("Part 1: {:?}", part1(include_str!("input.txt")));
-    println!("Part 2: {:?}", part2(include_str!("input.txt"), 50));
-}
+fn part2(input: &str, square_size: i32) -> i32 {
+    let (board, instructions) = input.split_once("\n\n").unwrap();
+    let board = parse_board(board);
+    let instructions = parse_instructions(instructions.trim());
 
-#[test]
-fn test_part1() {
-    assert_eq!(part1(include_str!("test.txt")), 6032);
+    let state = make_moves(&board, instructions, wrap_on_cube, square_size);
+    compute_password(state)
 }
 
 #[test]
 fn test_part2() {
     assert_eq!(part2(include_str!("test.txt"), 4), 5031);
+}
+
+fn main() {
+    println!("Part 1: {:?}", part1(include_str!("input.txt")));
+    println!("Part 2: {:?}", part2(include_str!("input.txt"), 50));
 }
